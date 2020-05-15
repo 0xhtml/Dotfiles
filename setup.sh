144906c68756c07fd3aa27c15bd472ac7eb25fe8
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-05-15 10:36:51 +0200

    [enh] Improve setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-cd ..
-ln -sf "Dotfiles/.bash_aliases"
-ln -sf "Dotfiles/.gitconfig"
-ln -sf "Dotfiles/.vimrc"
+DIR=$(pwd)
+cd $HOME
+ln -sf $DIR/.bash_aliases
+ln -sf $DIR/.gitconfig
+ln -sf $DIR/.vimrc
 cd .local/share/cura/3.1/definitions
-ln -sf ../../../../../Dotfiles/cura/geeetech_a10m.def.json
+ln -sf $DIR/cura/geeetech_a10m.def.json
 cd ../extruders
-ln -sf ../../../../../Dotfiles/cura/geeetech_a10m_extruder_0.def.json
-ln -sf ../../../../../Dotfiles/cura/geeetech_a10m_extruder_1.def.json
+ln -sf $DIR/cura/geeetech_a10m_extruder_0.def.json
+ln -sf $DIR/cura/geeetech_a10m_extruder_1.def.json
