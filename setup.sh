0e377e86875b31c29ec82f4afc1e4ad5a4f77136
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-05-15 10:25:19 +0200

    [enh] Move cura files into folder

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ ln -sf "Dotfiles/.bash_aliases"
 ln -sf "Dotfiles/.gitconfig"
 ln -sf "Dotfiles/.vimrc"
 cd .local/share/cura/3.1/definitions
-ln -sf ../../../../../Dotfiles/geeetech_a10m.def.json
+ln -sf ../../../../../Dotfiles/cura/geeetech_a10m.def.json
 cd ../extruders
-ln -sf ../../../../../Dotfiles/geeetech_a10m_extruder_0.def.json
-ln -sf ../../../../../Dotfiles/geeetech_a10m_extruder_1.def.json
+ln -sf ../../../../../Dotfiles/cura/geeetech_a10m_extruder_0.def.json
+ln -sf ../../../../../Dotfiles/cura/geeetech_a10m_extruder_1.def.json
