9484c7ed074940cbfe3c11321d88f7978be1980c
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-05-22 17:37:54 +0200

    [enh] Update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-DIR=$(pwd)
-cd $HOME
-ln -sf $DIR/.bash_aliases
-ln -sf $DIR/.gitconfig
-ln -sf $DIR/.vimrc
-cd .local/share/cura/3.1/definitions
-ln -sf $DIR/cura/geeetech_a10m.def.json
-cd ../extruders
-ln -sf $DIR/cura/geeetech_a10m_extruder_0.def.json
-ln -sf $DIR/cura/geeetech_a10m_extruder_1.def.json
+cp .bashrc $HOME
+cp .gitconfig $HOME
+cp .vimrc $HOME
+cp custom_geeetech_A10M.def.json $HOME/.local/share/cura/4.5/definitions
