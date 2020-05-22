c55db9757147a8d3029dacffe8adcf8a9554e720
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-05-22 17:45:48 +0200

    [add] Rofi

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cp .bashrc $HOME
 cp .gitconfig $HOME
-cp -r i3 $HOME/.config
 cp .vimrc $HOME
+cp -r i3 $HOME/.config
+cp -r rofi $HOME/.config
 cp custom_geeetech_A10M.def.json $HOME/.local/share/cura/4.5/definitions
