2d8ad280c81d68c856ca1a76b56f630ee02d22a2
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-07-04 14:25:51 +0200

    [add] Dunst for notifications

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ cp .bash_profile $HOME
 cp .gitconfig $HOME
 cp .vimrc $HOME
 cp .Xresources $HOME
+cp -r dunst $HOME/.config
 cp -r i3 $HOME/.config
 cp -r rofi $HOME/.config
 cp custom_geeetech_A10M.def.json $HOME/.local/share/cura/4.5/definitions
