e396a5cbfafc79e85ff1b63a7ca0f02d3e5dbdf5
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-12-16 10:16:35 +0100

    [add] xinitrc

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-cp .bashrc $HOME
 cp .bash_profile $HOME
-cp .gitconfig $HOME
-cp .vimrc $HOME
-cp .Xresources $HOME
+cp .bashrc $HOME
 cp -r dunst $HOME/.config
+cp .gitconfig $HOME
 cp -r i3 $HOME/.config
 cp -r rofi $HOME/.config
+cp .vimrc $HOME
+cp .xinitrc $HOME
+cp .Xresources $HOME
 cp custom_geeetech_A10M.def.json $HOME/.local/share/cura/4.7/definitions
