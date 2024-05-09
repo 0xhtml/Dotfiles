5e350ec3c37f68baea33742519845fc2815c64a7
0xhtml <34682885+0xhtml@users.noreply.github.com>
2024-05-09 14:57:08 +0200

    Make cow-(u)mount.sh more reliable

--- bin/cow-mount.sh
+++ bin/cow-mount.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <image> <location>"
+set -eu
+
+if [ -z ${1+x} ]; then
+    echo "Usage: $0 <image> <mountpoint>"
     exit 1
 fi
 
-if [ -z "$2" ]; then
-    echo "Usage: $0 $1 <location>"
+if [ -z ${2+x} ]; then
+    echo "Usage: $0 <image> <mountpoint>"
     exit 1
 fi
 
-sudo modprobe nbd max_part=8 && \
-sudo qemu-nbd --connect=/dev/nbd0 $1 && \
-mkdir -p $2 && \
-sudo mount /dev/nbd0p3 $2
+sudo modprobe nbd
+for i in $(seq 0 15); do
+    if lsblk -A /dev/nbd$i >/dev/null; then
+        cat /proc/$(cat /sys/class/block/nbd$i/pid)/cmdline | grep -q $1 || continue
+    else
+        sudo qemu-nbd --connect=/dev/nbd$i $1
+    fi
+    sudo partprobe /dev/nbd$i
+    sudo mount /dev/$(lsblk -Jb /dev/nbd$i | jq -r '.blockdevices[0].children | sort_by(.size)[-1].name') $2
+    exit 0
+done
+echo "No free nbd device found"
+exit 1
--- bin/cow-umount.sh
+++ bin/cow-umount.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-sudo umount /dev/nbd0p3
-sudo qemu-nbd --disconnect /dev/nbd0
-sudo rmmod nbd
+set -eu
+
+if [ -z ${1+x} ]; then
+    echo "Usage: $0 <mountpoint>"
+    exit 1
+fi
+
+DEV=$(df | grep $(realpath $1) | awk '{print $1}')
+sudo umount $1
+if lsblk -J ${DEV%p[0-9]*} | jq -e '.blockdevices[0].children|any(.mountpoints!=[null])' >/dev/null; then
+    exit 0
+fi
+sudo qemu-nbd --disconnect ${DEV%p[0-9]*}
+if ! lsblk -A | grep -q nbd; then
+    sudo rmmod nbd
+fi
